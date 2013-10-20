aab843eeba62dbfa0dda30d62b9dfb6d675492ff
devdigital <devdigital.cloud@gmail.com>
2013-10-20 20:17:14 +0100

    Enabled NuGet restore

---
+++ Organigram.Specs/OrganigramTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace Organigram.Specs
+{
+    using System.Net.Http;
+
+    public class OrganigramTests
+    {
+        [Fact]
+        public void OrganigramsApi_GetRequest_ReturnsSuccessStatusCode()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync("").Result;
+
+                Assert.True(
+                    response.IsSuccessStatusCode,
+                    "Actual status";
+            }
+        }
+    }
+}
