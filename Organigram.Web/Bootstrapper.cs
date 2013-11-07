2825ab4bd3e7518a9a8cdccb38e49ad76876ab2a
devdigital <devdigital.cloud@gmail.com>
2013-11-07 15:52:33 +0000

    Added Durandal

---
+++ Organigram.Web/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿namespace Organigram.Web
+{
+    using Nancy;
+    using Nancy.Conventions;
+
+    public class OrganigramBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            Conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("App"));
+        }
+    }
+}
