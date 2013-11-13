fbac9966e1aee19b9f49116b6a8abf2423ebe7d9
devdigital <devdigital.cloud@gmail.com>
2013-11-13 17:12:49 +0000

    Added TypeScript definition files

---
+++ Organigram.Web/App/viewmodels/welcome.ts
@@ -0,0 +1,7 @@
+/// <reference path="../../Scripts/typings/jquery/jquery.d.ts" />
+/// <reference path="../../Scripts/typings/knockout/knockout.d.ts" />
+declare var $;
+
+module test {
+   $('div').
+}
