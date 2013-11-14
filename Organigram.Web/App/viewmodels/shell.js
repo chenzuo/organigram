2c2647ec6345d0cf21c38add42bb111296f69c64
devdigital <devdigital.cloud@gmail.com>
2013-11-14 17:22:23 +0000

    Shell converted to TypeScript

--- Organigram.Web/App/viewmodels/shell.js
+++ Organigram.Web/App/viewmodels/shell.js
@@ -1,19 +1,20 @@
-﻿define(['plugins/router', 'durandal/app'], function (router, app) {
-    return {
-        router: router,
-        search: function() {
-            //It's really easy to show a message box.
-            //You can add custom options too. Also, it returns a promise for the user's response.
-            app.showMessage('Search not yet implemented...');
-        },
-        activate: function () {
-                       
+define(["require", "exports", 'plugins/router'], function(require, exports, __router__) {
+    var router = __router__;
+
+    var shell = (function () {
+        function shell() {
+        }
+        shell.prototype.activate = function () {
             router.map([
-                { route: '', title:'Welcome', moduleId: 'viewmodels/welcome', nav: true },
-                { route: 'flickr', moduleId: 'viewmodels/flickr', nav: true }
-            ]).buildNavigationModel();            
+                { route: '', title: 'Welcome', moduleId: 'viewmodels/welcome', nav: true }
+            ]).buildNavigationModel();
 
             return router.activate();
-        }
-    };
+        };
+        return shell;
+    })();
+    exports.shell = shell;
+
+    return new shell();
 });
+//# sourceMappingURL=shell.js.map
